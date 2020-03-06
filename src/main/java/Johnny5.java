d2fc93334af235bce0993aaf60495eb7bd1cf185
John Littlewood <john.littlewood@gmail.com>
2020-03-06 18:06:37 +0100

    Second attempt ...

--- src/main/java/Johnny5.java
+++ src/main/java/Johnny5.java
@@ -4,19 +4,12 @@ import robocode.ScannedRobotEvent;
 public class Johnny5 extends Robot {
 
     public void run() {
-
         while (true) {
-
-            ahead(100);
-
+            ahead(1);
+            turnRight(1);
             turnGunRight(360);
-
-            back(100);
-
-            turnGunRight(360);
-
+            
         }
-
     }
 
 

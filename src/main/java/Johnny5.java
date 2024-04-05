0ca9a7af53ea7e22cdae1ca109e73f7d2a0f1f4c
littlewoo <john@littlewood.se>
2024-04-05 18:15:26 +0200

    Added some behaviour

--- src/main/java/Johnny5.java
+++ src/main/java/Johnny5.java
@@ -1,5 +1,7 @@
 import robocode.*;
-//import java.awt.Color;
+
+import java.util.Random;
+import java.awt.Color;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -8,25 +10,37 @@ import robocode.*;
  */
 public class Johnny5 extends Robot
 {
+	Random rand;
+
 	/**
 	 * run: Johnny5's default behavior
 	 */
 	public void run() {
+		rand = new Random();
 		// Initialization of the robot should be put here
 
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		setColors(Color.PINK,Color.PINK,Color.PINK); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
+			doColours();
+
 			// Replace the next 4 lines with any behavior you would like
 			ahead(100);
+			turnLeft(rand.nextDouble()*180);
 			turnGunRight(360);
 			back(100);
+			turnRight(rand.nextDouble()*180);
 			turnGunRight(360);
 		}
+	}
+
+	private void doColours() {
+		Color colour = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
+		setColors(colour, colour, colour);
 	}
 
 	/**
@@ -44,12 +58,12 @@ public class Johnny5 extends Robot
 		// Replace the next line with any behavior you would like
 		back(10);
 	}
-	
+
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
 		back(20);
-	}	
+	}
 }
